50649aedf55bde3ecb3997255e029ed2ac88d645
Anna Yafi <annayafi@gmail.com>
2016-04-04 11:27:03 +0200

    problem with scope fixed when no scope

--- PofilesReader.MVC/LocalizedHelper.cs
+++ PofilesReader.MVC/LocalizedHelper.cs
@@ -15,7 +15,7 @@ namespace PofilesReader.MVC.Helpers
             var path = $"~/App_Data/Localization/{CultureInfo.CurrentUICulture}/";
             var paths = helper.ViewContext.HttpContext.Server.MapPath(path);
             var manager = new DefaultLocalizedStringManager(paths);
-            var t = manager.GetLocalizedString(text, text);
+            var t = manager.GetLocalizedString(string.Empty, text);
             return new MvcHtmlString(t);
         }
         public static MvcHtmlString T(this HtmlHelper helper, string text, string context)
